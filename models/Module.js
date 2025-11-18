25be6da83f9c2f07cc4dd09caf3d2cdfcfdd5b4a
DylanFourie1996 <143059350+DylanFourie1996@users.noreply.github.com>
2025-11-18 13:18:45 +0200

    Update Module.js

--- models/Module.js
+++ models/Module.js
@@ -1,25 +1,12 @@
-const db = global.db;
+const express = require('express');
+const router = express.Router();
+const moduleController = require('../controllers/moduleController');
+const auth = require('../middleware/auth'); // optional if you want auth
 
-exports.createModule = async (userId, title, description) => {
-    const docRef = await db.collection('modules').add({
-        userId,
-        title,
-        description: description || '',
-        createdAt: new Date(),
-    });
-    return docRef.id;
-};
+// Routes
+router.post('/', moduleController.createModule);
+router.get('/', moduleController.getModules);
+router.put('/:id', moduleController.updateModule);
+router.delete('/:id', moduleController.deleteModule);
 
-exports.getModulesByUser = async (userId) => {
-    const snapshot = await db.collection('modules').where('userId', '==', userId).get();
-    return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-};
-
-exports.updateModule = async (moduleId, data) => {
-    await db.collection('modules').doc(moduleId).update(data);
-};
-
-exports.deleteModule = async (moduleId) => {
-    await db.collection('modules').doc(moduleId).delete();
-};
-// --- End of model ---
+module.exports = router;
